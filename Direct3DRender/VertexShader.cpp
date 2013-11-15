da86e492fb53adb90483a066bbdbc1c45d740fd5
ArtemijStrizhakov <Artemij.Strizhakov@mail.ru>
2013-11-15 23:27:22 +0600

    remove rem blocks

--- Direct3DRender/VertexShader.cpp
+++ Direct3DRender/VertexShader.cpp
@@ -54,16 +54,6 @@ bool CVertexShader::Compile( std::wstring const & shaderPath, ID3D11Device* pDev
 
 	ShaderUtils::CreateInputLayoutDescFromVertexShaderSignature(pVSBlob, pDevice, &m_pVertexLayout);
 
-	/*D3D11_INPUT_ELEMENT_DESC layout[] =
-	{
-		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-	};
-	UINT numElements = ARRAYSIZE( layout );
-
-	// Create the input layout
-	hr = pDevice->CreateInputLayout( layout, numElements, pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), &m_pVertexLayout );
-	*/
 	pVSBlob->Release();
 	
 	if(FAILED(hr))
